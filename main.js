7c6c98ad47b6e836eb88a519ced788836764fd72
Trevor Brooks <tbrooks7@NYMACLTTBROO1.local>
2016-01-11 23:40:18 -0500

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+	$('.container>img').css('width','200px');
+	$( 'li' ).show();
+	$('h1').css('color','black');
+	$('#anvil').animate({height: '300px', opacity: '0.4'}, "slow");
+	$('ul>li a').eq('0')
+		.attr('href', 'http://www.google.com')
+		.html('Google');
+
